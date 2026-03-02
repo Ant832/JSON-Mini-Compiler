d20c918edb90721578b61039f82f311fb2266260
AnthonyJ <antnyjank@gmail.com>
2026-03-02 16:30:16 -0700

    bugfix ] -> RSQUARE

--- include/recognizer.hpp
+++ include/recognizer.hpp
@@ -84,7 +84,7 @@ class Parser {
     }
 
     void array() {
-        if (tokenVec[position] == "]") {
+        if (tokenVec[position] == "RSQUARE") {
             return;
         }
         value();
