b630e826783b2601b800418442ebad151089c167
AnthonyJ <antnyjank@gmail.com>
2026-03-30 19:30:46 -0600

    removed pretty printer

--- include/ast.hpp
+++ include/ast.hpp
@@ -60,16 +60,4 @@ public:
     void visit(JSONObject& node) override;
 };
 
-
-class JSONPrettyPrinter : public JSONVisitor {
-    bool start = true;
-    int depth = -1;
-    int objects = 0;
-public:
-    void printIndent();
-    void visit(JSONValue& node) override;
-    void visit(JSONArray& node) override;
-    void visit(JSONObject& node) override;
-};
-
 #endif
